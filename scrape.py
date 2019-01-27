837681e64b482586023053918edf6b8cb47704b1
eonofrey <38504767+eonofrey@users.noreply.github.com>
2019-01-26 19:06:42 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -8,24 +8,7 @@ from selenium import webdriver
 import time
 import getpass
 
-# Input Password
-password = getpass.getpass()
-
-# Make it so chrome will allow automated software
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-infobars")
-
-# Find driver, add argument for the options we just created 
-driver = webdriver.Chrome('/Users/Eric/Downloads/chromedriver_4',chrome_options=chrome_options)
-
-# Grab site
-driver.get('https://www.chess.com/games/archive')
-
-# Sign in 
-driver.find_element_by_xpath('//*[@id="username"]').send_keys("eonofrey@gmail.com")
-driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
-driver.find_element_by_xpath('//*[@id="login"]').click()
-
+#################### Define Functions ####################
 def pull_results(link = None):
     # Grab link 
     driver.get(link)
@@ -112,4 +95,44 @@ def pull_dates(link = None):
     
     return clean_dates
 
+#################### Sign In to chess.com ####################
+# Input Password
+password = getpass.getpass()
 
+# Make it so chrome will allow automated software
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-infobars")
+
+# Find driver, add argument for the options we just created 
+driver = webdriver.Chrome('/Users/Eric/Downloads/chromedriver_4',chrome_options=chrome_options)
+
+# Grab site
+driver.get('https://www.chess.com/games/archive')
+
+# Sign in 
+driver.find_element_by_xpath('//*[@id="username"]').send_keys("eonofrey@gmail.com")
+driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
+driver.find_element_by_xpath('//*[@id="login"]').click()
+
+#################### Start the Scrape ####################
+# Make the containers
+results_container = []
+moves_container = []
+links_container = []
+dates_container = []
+
+for page_num in list(range(1,12)):
+    # Make the Link
+    link = """https://www.chess.com/games/archive?gameOwner=my_game&gameTypes%5B0%5D
+    =lightning&gameTypes%5B1%5D=bullet&gameTypes%5B2%5D=blitz&gameTypes%5B3%5D
+    =standard&gameTypes%5B4%5D=liveChess960&gameTypes%5B5%5D=
+    bughouse&gameTypes%5B6%5D=threecheck&gameTypes%5B7%5D=crazyhouse&gameTypes%5B8%5D
+    =kingofthehill&gameTypes%5B9%5D=rapid&gameType=live&page=""" + str(page_num)
+    
+    results_container = results_container + pull_results(link)
+    moves_container = moves_container + pull_moves(link)
+    links_container = links_container + pull_links(link)
+    dates_container = dates_container + pull_dates(link)
+    
+time.sleep(3)
+driver.close() 
